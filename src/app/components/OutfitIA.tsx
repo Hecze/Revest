72e2f5d14d5ab2ae8df77a011e132ff8ee981ba3
Hecze <hectorzerrillo85@gmail.com>
2024-09-15 13:44:26 -0500

    OutfitAI mejorado y responsive

--- src/app/components/OutfitIA.tsx
+++ src/app/components/OutfitIA.tsx
@@ -23,7 +23,7 @@ interface OutfitImageProps {
 }
 
 const OutfitImage: React.FC<OutfitImageProps> = ({ src, alt, className }) => (
-  <div className={`bg-gray-300 relative aspect-w-1" ${className}`} style={{ paddingBottom: '100%' }}>
+  <div className={`bg-gray-300 relative shadow-md cursor-pointer hover:opacity-90 transition  md:col-span-2 " ${className}`} style={{ paddingBottom: '100%' }}>
     <Image src={src} alt={alt} layout="fill" objectFit="cover" className="rounded-md" />
   </div>
 );
@@ -35,16 +35,16 @@ interface TextBoxProps {
 }
 
 const TextBox: React.FC<TextBoxProps> = ({ text, className }) => (
-  <div className={`bg-white p-4 shadow-md rounded-md w-full text-center ${className}`}>
+  <div className={` p-4 px-8  shadow-md cursor-pointer hover:opacity-90 transition w-full text-center ${className}`}>
     {text}
   </div>
 );
 
 // Grid Layout Component
 const GridLayout: React.FC = () => (
-  <div className="grid  grid-cols-2 lg:grid-cols-3 gap-4 w-11/12 max-w-screen-lg">
+  <div className="grid grid-cols-2 md:grid-cols-7 gap-2 md:w-3/5 max-w-screen-md mt-6">
     {/* Large Image (1 col, 3 rows) */}
-    <div className={`bg-gray-300 relative aspect-w-1" col-span-2 lg:col-span-1 row-span-3`} style={{ paddingBottom: '65%' }}>
+    <div className={`bg-gray-300 relative  shadow-md cursor-pointer hover:opacity-90 transition col-span-2 md:col-span-3  row-span-3 h-96 md:h-auto md:aspect-w-1 `} style={{ paddingBottom: '200%' }}>
       <Image src={"/IA-completo.png"} alt={"Large Outfit"} layout="fill" objectFit="cover" className="rounded-md" />
     </div>
 
@@ -67,10 +67,10 @@ const GridLayout: React.FC = () => (
     />
 
     {/* Text Boxes */}
-    <div className="col-span-2 bg-gray-200 flex flex-col justify-center items-center p-6">
-      <div className="flex flex-col w-full justify-around items-center gap-4">
+    <div className="col-span-2 md:col-span-4 flex flex-col justify-center items-center ">
+      <div className="flex flex-col w-full justify-around items-center gap-2">
         <TextBox text="Suscríbete desde 5 prendas por mes y úsalas cuando quieras" className='bg-slate-900 text-white' />
-        <TextBox text="Elige entre más de 10 mil prendas en catálogo y ayuda a nuestro planeta" />
+        <TextBox text="Elige entre más de 10 mil prendas en catálogo y ayuda a nuestro planeta" className='bg-white text-slate-900' />
       </div>
     </div>
   </div>
@@ -79,7 +79,7 @@ const GridLayout: React.FC = () => (
 // Main Component
 const OutfitIA: React.FC = () => {
   return (
-    <section className="flex flex-col items-center py-10 bg-gray-100">
+    <section className="flex flex-col items-center py-10">
       <Title />
       <Input />
       <GridLayout />
