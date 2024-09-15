aa24340e463e7f310f560017f0c8540979b04462
Hecze <hectorzerrillo85@gmail.com>
2024-09-15 01:35:04 -0500

    AccesoriosCalzado ahora es mas responsive

--- src/app/components/AccesoriosCalzado.tsx
+++ src/app/components/AccesoriosCalzado.tsx
@@ -3,15 +3,15 @@ import Image from 'next/image';
 // Componente para productos grandes (ocupan 3 columnas)
 const ProductoGrande: React.FC<{ imgSrc: string; title: string; description: string; bgColor: string }> = ({ imgSrc, title, description, bgColor }) => {
   return (
-    <div className={`col-span-6 md:col-span-3 ${bgColor} p-8 flex flex-row justify-between px-24`}>
+    <div className={`col-span-2 sm:col-span-4 lg:col-span-3 ${bgColor} p-8 flex flex-row justify-center md:justify-between lg:px-24`}>
       <Image
         src={imgSrc}
         alt={title}
         width={150}
         height={150}
-        className="mb-4"
+        className="mb-4 hidden md:block"
       />
-      <div className='flex flex-col justify-center items-center'>
+      <div className='flex flex-col justify-center items-center text-center'>
         <h2 className="text-2xl font-bold">{title}</h2>
         <p className="mt-2">{description}</p>
         <button className="mt-6 bg-black text-white py-2 px-4 hover:bg-gray-700 w-64">
@@ -26,7 +26,7 @@ const ProductoGrande: React.FC<{ imgSrc: string; title: string; description: str
 // Componente para productos pequeños (ocupan 1 columna)
 const ProductoPequeno: React.FC<{ imgSrc: string; altText: string }> = ({ imgSrc, altText }) => {
   return (
-    <div className="col-span-3 md:col-span-1 bg-red-200">
+    <div className="col-span-1 bg-red-200">
       <Image
         src={imgSrc}
         alt={altText}
@@ -43,7 +43,7 @@ const ProductoPequeno: React.FC<{ imgSrc: string; altText: string }> = ({ imgSrc
 const AccesoriosCalzado: React.FC = () => {
   return (
     <section className="bg-gray-100 py-10  px-4 sm:px-12 md:px-24 xl:px-36">
-      <div className="grid grid-cols-5 gap-2 ">
+      <div className="grid grid-col-2 sm:grid-col-4 lg:grid-cols-5 gap-2 ">
 
         {/* Primer div grande (Accesorios) */}
         <ProductoGrande
