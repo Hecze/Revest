18dc0fafc1296d4bf50a543a773b6ae02c14cc3d
Hecze <hectorzerrillo85@gmail.com>
2024-09-16 00:35:25 -0500

    Página Membresia creada

---
+++ src/app/membership/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function MembershipPage(): JSX.Element {
+  return (
+    <div>
+      <h1>Membership Page</h1>
+      <p>Welcome to the membership page.</p>
+    </div>
+  );
+}
+
